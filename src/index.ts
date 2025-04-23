d919c879005cc9cb6e9ce2aeb2e2d6070b59cc22
khj0311 <41286402+khj0311@users.noreply.github.com>
2025-04-23 14:29:54 +0900

    Add src/index.ts as main entry point

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Components export
+// 아직 컴포넌트가 구현되지 않았으므로 주석 처리
+// export * from './components/Inputs/Autocomplete';
+// export * from './components/DataDisplay/Avatar';
+// export * from './components/Feedback/Alert';
+
+// Types export
+export * from './types';
