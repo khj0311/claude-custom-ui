43ff9dc5486ded2702d7bf4f06382f7bddd08a21
khj0311 <41286402+khj0311@users.noreply.github.com>
2025-04-23 15:07:07 +0900

    feat: Update index.ts to export Accordion component

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export * from './components/Inputs/Autocomplete';
 export * from './components/DataDisplay/Avatar';
 export * from './components/Feedback/Alert';
+export * from './components/Surfaces/Accordion';
 
 // Types export
 export * from './types';
