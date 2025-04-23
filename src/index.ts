d936307b01f741faae7da71bf70588c6cf225f92
khj0311 <41286402+khj0311@users.noreply.github.com>
2025-04-23 14:33:34 +0900

    Update src/index.ts to export Avatar component

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Components export
 export * from './components/Inputs/Autocomplete';
-// export * from './components/DataDisplay/Avatar';
+export * from './components/DataDisplay/Avatar';
 // export * from './components/Feedback/Alert';
 
 // Types export
